a04dbc1e6ab18c20bc68b65e1a581a981243f0ba
miykael <michaelnotter@hotmail.com>
2015-08-08 22:05:48 +0200

    ENH: temporary start screen added

--- STEN.py
+++ STEN.py
@@ -6,15 +6,42 @@ Start STEN and open the GUI
 """
 
 
-class STEN(wx.App):
+class StartSTEN(wx.App):
+
+    """Calls STEN Splash Screen"""
 
     def OnInit(self):
-        fen = MainFrame()
-        fen.Show(True)
-        self.SetTopWindow(fen)
+        STENSplash = STENSplashScreen()
+        STENSplash.Show()
         return True
+
+
+class STENSplashScreen(wx.SplashScreen):
+
+    """Opens STEN Splash Screen and after exit starts STEN"""
+
+    def __init__(self, parent=None):
+        aBitmap = wx.Image(name="STEN_logo.png").ConvertToBitmap()
+        splashStyle = wx.SPLASH_CENTRE_ON_SCREEN | wx.SPLASH_TIMEOUT
+        splashDuration = 1000
+        wx.SplashScreen.__init__(self, aBitmap, splashStyle,
+                                 splashDuration, parent)
+        self.Bind(wx.EVT_CLOSE, self.OnExit)
+        wx.Yield()
+
+    def OnExit(self, evt):
+        self.Hide()
+        MyFrame = MainFrame()
+        app.SetTopWindow(MyFrame)
+        MyFrame.Show(True)
+        evt.Skip()
 
 # Opens the GUI if script is run directly
 if __name__ == "__main__":
-    app = STEN()
+    app = StartSTEN()
     app.MainLoop()
+
+
+# TODO: For More Logo's Check
+# Logo Maker - http://logotypemaker.com/logo-maker
+# Design Mantic - www.designmantic.com/logo-design/samples
