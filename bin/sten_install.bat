196c3c906299abd857dd2c2b233a842b24d78a2f
miykael <michaelnotter@hotmail.com>
2018-01-18 11:03:26 +0100

    WIP: for installation

---
+++ bin/sten_install.bat
@@ -0,0 +1,9 @@
+conda install -y wxpython
+conda update -y scipy numpy tables wxpython
+pip install --upgrade pip rpy2
+pip install --upgrade sten
+ipython --c "import sten; sten.gui()"
+
+http://www.r-bloggers.com/how-to-do-a-silent-install-of-r/
+
+Install current R version
